1987d5ada84fc4446cccf0575697253e2b48ed8e
Ajah Emmanuel Nnanna <nnannamarach4@gmail.com>
2025-02-19 13:45:10 +0100

    Wrote composable row and columns

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,15 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +40,6 @@ class MainActivity : ComponentActivity() {
                             name = "Android, this is my first",
                             modifier = Modifier.padding(innerPadding)
                         )
-                        CustomText(text = "I just go started with Kotlin and jetpack")
                     }
 
                 }
@@ -45,22 +58,38 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+
+@Composable
+fun RowScope.CustomItem(weight: Float, color: Color){
+    Surface(
+        modifier = Modifier
+            .width(200.dp)
+            .height(50.dp)
+            .weight(weight),
+        color = color
+    ) { }
+}
+
+
+@Preview(showBackground = true, name = "Emmanuel")
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Column {
-            Greeting("Android")
-            CustomText(text = "I just go started with Kotlin and jetpack")
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            CustomItem(weight = 3f, color = MaterialTheme.colorScheme.primary)
+            CustomItem(weight = 2f, color = MaterialTheme.colorScheme.secondary)
         }
-
     }
 }
 
 
-@Composable
-fun CustomText(text: String){
-    Text(
-        text = text,
-    )
-}
+//@Composable
+//fun CustomText(text: String){
+//    Text(
+//        text = text,
+//    )
+//}
